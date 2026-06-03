b130817482dd9f19242436e789eb2d90710d5e99
hayato-yokoyama <65605945+ hayato-yokoyama@users.noreply.github.com>
2026-06-03 09:05:50 +0900

    ➕ tsrxプラグインを使用するための設定を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tsrxReact from '@tsrx/vite-plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsrxReact()],
 })
